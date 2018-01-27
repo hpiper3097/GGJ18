8195b8052dd61bb1616ea0cb983ea7826acfa2a7
Hunter Galtier <huntrjg@gmail.com>
2018-01-27 14:14:19 -0600

    patrol feature (#9)
    This reverts commit b8e0d2fcd37671749dda0a729b64c7510060e644.
    
    * door behavior update
    
    * pathing feature

--- Assets/Scripts/oneTimeDoorBehavior.cs
+++ Assets/Scripts/oneTimeDoorBehavior.cs
@@ -1,98 +1,82 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class DoorBehaviour : MonoBehaviour {
+public class oneTimeDoorBehavior : MonoBehaviour
+{
 
     private bool isOpen;
     private bool isOpening;
     private bool isClosing;
     private float alphaLevel = .1f;
-    public string sceneName;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         isOpen = false;
         isOpening = false;
         isClosing = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         //if door is opening 
-		if(isOpening)
+        if (isOpening)
         {
             //and door is not all the way open
             if (!isOpen)
             {
                 Vector4 color = gameObject.GetComponent<MeshRenderer>().material.color;
                 //keep opening as long as the door is visible
-                if (color.w >= 0)
+                if (color.w > 0)
                 {
                     gameObject.GetComponent<MeshRenderer>().material.color = new Vector4(color.x, color.y, color.z, color.w - alphaLevel);
-                    gameObject.GetComponent<Renderer>().material.color = new Vector4(color.x, color.y, color.z, color.w - alphaLevel);
-
                 }
-                else //set the door to open. no longer opening.
+                //else set the door to open. no longer opening.
+                else
                 {
-                    gameObject.GetComponent<MeshCollider>().isTrigger = true;
+                    gameObject.GetComponent<BoxCollider>().isTrigger = true;
                     isOpen = true;
                     isOpening = false;
-                    
                 }
 
-            }
-            else //door is opening and open
-            {
-                //do nothing because player is inside the doorway
             }
         }
         //else the door is closing
         else if (isClosing)
         {
-            gameObject.GetComponent<MeshCollider>().isTrigger = false;
             Vector4 color = gameObject.GetComponent<MeshRenderer>().material.color;
+            gameObject.GetComponent<MeshRenderer>().material.color = new Vector4(color.x, color.y, color.z, color.w + alphaLevel);
             //if the door is still closing
             if (color.w <= 1)
             {
-                gameObject.GetComponent<MeshRenderer>().material.color = new Vector4(color.x, color.y, color.z, color.w + alphaLevel);
+                gameObject.GetComponent<BoxCollider>().isTrigger = false;
             }
-            else //the door has completely closed.
+            //else the door has completewly closed.
+            else
             {
                 isOpen = false;
                 isClosing = false;
-                //change scenes if one is provided
-                if (sceneName != null)
-                    SceneManager.LoadScene(sceneName);
             }
         }
-        
-	}
-    
-    private void OnCollisionEnter(Collision other)
+
+    }
+
+
+
+
+    private void OnCollisionStay(Collision other)
     {
         if (other.gameObject.tag == "Controllable")
         {
             isOpening = true;
         }
     }
-    private void OnCollisionExit(Collision collision)
-    {
-        isOpen = false;
-        if(isOpening)
-        {
-            isOpening = false;
-            isClosing = true;
-        }
-        
-        
-    }
 
     private void OnTriggerExit(Collider collision)
     {
         isClosing = true;
         isOpen = false;
-        
     }
 }
