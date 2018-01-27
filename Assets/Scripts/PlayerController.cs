73ac278d7c0bb37a6102a2b73fe634796cc11dda
AverageJoe82 <tserpas1289@gmail.com>
2018-01-27 06:28:11 -0600

    added a watchbox feature

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,17 +9,30 @@ public class PlayerController : MonoBehaviour {
     private CharacterComponent c;
     public bool active;
 
+    //values used to keep track of watching
+    public float watchSpeed;
+    private int watching;
+    private SpriteRenderer sprite;
+    private Vector4 colors;
+
 	// Use this for initialization
 	void Start () {
         rb = gameObject.GetComponent<Rigidbody>();
         m = GameObject.FindWithTag("Manager").GetComponent<Manager>();
         c = gameObject.GetComponent<CharacterComponent>();
+
+        //initialize values related to watching
+        watching = 0;
+        sprite = transform.GetChild(0).GetComponent<SpriteRenderer>(); //bad practice, should try to assign this in the editor but it's a game jam so im not rdoing this
+        colors = sprite.color;
+        sprite.enabled = false;
 	}
 	
-	// Update is called once per frame
+	// FixedUpdate is called a fixed number of times per frame
 	void FixedUpdate () {
         if (active)
         {
+            //movement
             float moveV = Input.GetAxis("Vertical");
             float moveH = Input.GetAxis("Horizontal");
             Vector3 movement = new Vector3(moveH, rb.velocity.y/c.speed, moveV);
@@ -30,7 +43,29 @@ public class PlayerController : MonoBehaviour {
                 m.EntityChangeA(gameObject);
                 rb.velocity = Vector3.zero;
             }
+            //handle being watched
+            if (watching != 0)
+                sprite.color = new Vector4(colors.x, colors.y, colors.z, colors.w - watchSpeed);
+            else if (colors.w < 1)
+                sprite.color = new Vector4(colors.x, colors.y, colors.z, colors.w + watchSpeed);
+            else
+                sprite.enabled = false;
         }
+    }
+    
+    //keep a count of the number of things watching the player
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Watch")
+        {
+            watching++;
+            sprite.enabled = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Watch")
+            watching--;
     }
 
     public void SetState(bool boo)
