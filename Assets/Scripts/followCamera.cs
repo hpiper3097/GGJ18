22739c419c05936543c4857455da3144b2cdfffb
AverageJoe82 <tserpas1289@gmail.com>
2018-01-27 16:58:17 -0600

    jun's camera and persistent deaths

---
+++ Assets/Scripts/followCamera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class followCamera : MonoBehaviour
+{
+
+    public string target;
+    public float damping = 1;
+    Vector3 offset;
+    
+    void Start()
+    {
+        offset = transform.position - GameObject.FindWithTag(target).transform.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = GameObject.FindWithTag(target).transform.position + offset;
+        Vector3 position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * damping);
+        transform.position = position;
+
+        transform.LookAt(GameObject.FindWithTag(target).transform.position);
+    }
+}
