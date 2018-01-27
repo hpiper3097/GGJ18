b125246eff3ceae353f72c0e604f9a10d856f4dd
Hunter Galtier <huntrjg@gmail.com>
2018-01-27 09:38:38 -0600

    Revert "fuk u"
    This reverts commit b8e0d2fcd37671749dda0a729b64c7510060e644.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,27 +2,76 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
 
-    //public int num;
+    private Rigidbody rb;
+    private Manager m;
+    private CharacterComponent c;
+    public bool active;
+
+    //values used to keep track of watching
+    public float watchSpeed;
+    private int watching;
+    private SpriteRenderer sprite;
+    private Vector4 colors;
 
 	// Use this for initialization
-	void Awake() { 
-        int num = 1;
+	void Start () {
+        rb = gameObject.GetComponent<Rigidbody>();
+        m = GameObject.FindWithTag("Manager").GetComponent<Manager>();
+        c = gameObject.GetComponent<CharacterComponent>();
+
+        //initialize values related to watching
+        watching = 0;
+        sprite = transform.GetChild(0).GetComponent<SpriteRenderer>(); //bad practice, should try to assign this in the editor but it's a game jam so im not rdoing this
+        colors = sprite.color;
+        sprite.enabled = false;
 	}
 	
-	// Update is called once per frame
+	// FixedUpdate is called a fixed number of times per frame
 	void FixedUpdate () {
-		
-	}
-
-    private void OnMouseDown()
+        if (active)
+        {
+            //movement
+            float moveV = Input.GetAxis("Vertical");
+            float moveH = Input.GetAxis("Horizontal");
+            Vector3 movement = new Vector3(moveH, rb.velocity.y/c.speed, moveV);
+            rb.velocity = movement * c.speed;
+            //swap on space
+            if(Input.GetKeyDown("space"))
+            {
+                m.EntityChangeA(gameObject);
+                rb.velocity = Vector3.zero;
+            }
+            //handle being watched
+            if (watching != 0)
+                sprite.color = new Vector4(colors.x, colors.y, colors.z, colors.w - watchSpeed);
+            else if (colors.w < 1)
+                sprite.color = new Vector4(colors.x, colors.y, colors.z, colors.w + watchSpeed);
+            else
+                sprite.enabled = false;
+        }
+    }
+    
+    //keep a count of the number of things watching the player
+    void OnTriggerEnter(Collider other)
     {
-        
+        if(other.gameObject.tag == "Watch")
+        {
+            watching++;
+            sprite.enabled = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Watch")
+            watching--;
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    public void SetState(bool boo)
     {
-        
+        active = boo;
     }
 }
+
+
