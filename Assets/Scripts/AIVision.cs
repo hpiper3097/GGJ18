e3a85d2fa2a5cc459c719b47f9fbcee6c851b5e9
hpiper3097 <hpiper3097@gmail.com>
2018-01-27 23:56:01 -0600

    new stuff

---
+++ Assets/Scripts/AIVision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIVision : MonoBehaviour {
+
+    private SphereCollider col;
+
+	// Use this for initialization
+	void Start ()
+    {
+        col = gameObject.GetComponent<SphereCollider>();
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //if(check FOV)
+            //if(raycast)
+            //SEEn!
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/TargetTake.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetTake : MonoBehaviour {
+
+    // Use this for initialization
+
+    private Manager m;
+
+	void Start () {
+        m = GameObject.FindWithTag("Manager").GetComponent<Manager>();
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (gameObject.tag == "Player")
+        {
+            if (other.gameObject.tag == "Controllable" && other.gameObject == m.nearestEntity)
+            {
+                other.gameObject.GetComponent<Light>().intensity = 4;
+            }
+            else if(other.gameObject.tag == "Controllable")
+                other.gameObject.GetComponent<Light>().intensity = 0;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(gameObject.tag == "Player")
+        {
+            if(other.gameObject.tag == "Controllable")
+            {
+                other.gameObject.GetComponent<Light>().intensity = 0;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
